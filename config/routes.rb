2a82dff46a321812a6fda562b37ea5973f8a67cc
flcamp0217 <fl.it.72183@gmail.com>
2024-01-04 15:38:30 +0900

    データベース設計

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+#まえのやつ！
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+Rails.application.routes.draw do
+  root to: "items#index" #(ルートにアクセスするとindexアクションに行く)
+  resources :items 
 end
