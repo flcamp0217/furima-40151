3c424669ff387ce814d34f3e52cb20a981494f98
flcamp0217 <fl.it.72183@gmail.com>
2024-01-05 17:20:17 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :nickname, presence: true
+  validates :kanji_name, presence: true
+  validates :kana_name, presence: true
+  validates :birth_day, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+# 半角英数字混合での入力が必須であることの実装
+  validate :password_complexity
+
+  def password_complexity
+   return if password.blank? || password =~ /^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$/m
+   errors.add :password, "は半角英数字の組み合わせで、最低6文字以上で入力してください"
+  end
 end
