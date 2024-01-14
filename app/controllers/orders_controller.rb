02fb38bf6a375ba7fc1893cc560c09ff191a93f6
flcamp0217 <fl.it.72183@gmail.com>
2024-01-14 19:39:52 +0900

    Formオブジェクト完成(仮）

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,24 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
+  def index
+    @item = Item.includes(:user).find(params[:item_id])
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(purchase_params)
+    if @order.valid?
+       @order.save
+       redirect_to items_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:order).permit(:postcode, :shipping_area_id, :city, :street_address, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,14 +1,14 @@
 class Order
   include ActiveModel::Model
-  attr_accessor :user, :item, :postcode, :shipping_area_id, :city, :street_address, :building, :phone, :user_id
+  attr_accessor :user, :item, :postcode, :shipping_area_id, :city, :street_address, :building, :phone
 
   with_options presence: true do
     validates :user_id
     validates :item_id
     validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :shipping_area_id, numericality: {other_than: 1, message: "can't be blank"}
-    validates :city, {only_integer: true}
-    validates :street_address, {only_integer: true}
+    validates :city, numericality: { only_integer: true }
+    validates :street_address, numericality: { only_integer: true }
     validates :phone, format: {with: /\A\d+\z/, message: 'is invalid'}
   end
   validates :building, numericality: {only_integer: true}
