b9c0da37539b2919ab3921a4cfc4b3fde5c3ec58
flcamp0217 <fl.it.72183@gmail.com>
2024-01-18 20:09:39 +0900

    修正一(リファクタリング

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: :index
+  before_action :set_item, only: [:index, :create]
   before_action :move_to_top, only: :index
-
   def index
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order = Order.new
@@ -14,7 +14,6 @@ class OrdersController < ApplicationController
       @order.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render 'index', status: :unprocessable_entity
     end
@@ -29,7 +28,6 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    @item = Item.find(params[:item_id])
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,
@@ -38,8 +36,11 @@ class OrdersController < ApplicationController
     )
   end
 
-  def move_to_top
+  def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def move_to_top
     if current_user.id == @item.user.id || @item.purchase.present?
     redirect_to root_path
     end
